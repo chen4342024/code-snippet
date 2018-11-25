40097dacfbe1b2f5c9dc1cd862f09d6966fadc0d
chen_jia_jia <chenjiajia@zuzuche.com>
2018-11-25 18:27:37 +0800

    build

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,16 +4,13 @@ module.exports = {
     description: '常用代码片段收集',
     themeConfig: {
         lastUpdated: '上次更新', // string | boolean
-        nav: [
-            { text: '个人博客', link: 'https://chen4342024.github.io/new-blog/' },
-        ],
-        locales: {
-            // 键名是该语言所属的子路径
-            // 作为特例，默认语言可以使用 '/' 作为其路径。
-            '/': {
-                lang: 'zh-CN', // 将会被设置为 <html> 的 lang 属性
-            }
-        },
+        nav: [{
+            text: '更多',
+            items: [
+                { text: '个人博客', link: 'https://chen4342024.github.io/new-blog/' },
+                { text: 'GitHub', link: 'https://github.com/chen4342024' },
+            ]
+        }],
         sidebar: [{
                 title: 'JS 相关',
                 collapsable: false,
